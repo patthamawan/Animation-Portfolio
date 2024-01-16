835d114ed1f295c7b5dabb030e9de63730d55949
Nunny Iteeyaporn <nuninator@Nunnys-Mac-mini.cust.communityfibre.co.uk>
2024-01-16 11:02:30 +0000

    reel update

--- src/components/AnimReel.js
+++ src/components/AnimReel.js
@@ -7,7 +7,7 @@ const VideoFile = () => {
   const [password, setPassword] = useState('');
   const [isPasswordCorrect, setIsPasswordCorrect] = useState(false);
   const [isShaking, setIsShaking] = useState(false);
-  const videoUrl = '/DemoReelDec2023_cartoon.mov';
+  const videoUrl = '/DemoReelDec2023_v2.mov';
   
 
   const handlePasswordChange = (e) => {
